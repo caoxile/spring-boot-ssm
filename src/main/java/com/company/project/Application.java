4797cfbf1a3a87506370ef62f17f88467427a7ab
caoxile <caoxile@gmail.com>
2018-07-11 12:34:24 +0800

    Init Project

---
+++ src/main/java/com/company/project/Application.java
@@ -0,0 +1,14 @@
+package com.company.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.company.project.dao")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
